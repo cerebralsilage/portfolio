23a2281b53bb36fb0a46a2dd2cfaa16156d8348c
cerebralsilage <michaelploplis@gmail.com>
2022-07-15 16:31:54 -0700

    initial

---
+++ Public/readme.md
@@ -0,0 +1,6 @@
+# Portfolio
+
+Welcome to my portfolio.  Feel free to click on the links and explore my websites.  Contact me if you have any questions or opportunities.
+
+## License:
+GNU V3
---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+console.log(process.env.Pass)
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.static('public'));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/Public/pages/contact.html')
+});
+
+app.post('/', (req, res) => {
+  console.log(req.body);
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'mikesportfoliocontact@gmail.com',
+      pass: `${process.env.Pass}`
+    }
+  })
+  const mailOptions = {
+    from: req.body.email,
+    to: 'mikesportfoliocontact@gmail.com',
+    subject: `Message from ${req.body.email}: ${req.body.subject}`,
+    text: req.body.message
+  }
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.send('error');
+    } else  {
+      console.log('Email sent: ' + info.response);
+      res.send('success');
+    }
+  })
+});
+
+app.listen(PORT, ()=>{
+  console.log(`Server running on port ${PORT}`)
+});
